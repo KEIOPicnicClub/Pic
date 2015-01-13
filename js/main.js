1ad4765e783aeb88db44528612cba2f4a68de6b8
Pen-Guin <nuno0825@gmail.com>
2015-01-13 14:16:16 +0900

    add fade effect

--- js/main.js
+++ js/main.js
@@ -68,6 +68,7 @@ $(function () {
                     .attr('data-gallery', '')
                     .appendTo(linksContainer);
             });
+            initFadeSetting(true);
         });
     }
 
@@ -115,6 +116,7 @@ $(function () {
                     .attr('data-gallery', '')
                     .appendTo(linksContainer);
             });
+            initFadeSetting(true);
         });
     }
 
@@ -137,7 +139,106 @@ $(function () {
         console.log("setPhotoWidth");
 
         $('#links img').fadeOut(function() {
-            $('#links img').css("width", width).fadeIn();
+            $('#links img').css("width", width);
+        });
+        if(width == "10%") {
+            initFadeSetting(false, 10);
+        }
+        else if(width == "25%") {
+            initFadeSetting(false, 4);
+        }
+        else {
+            initFadeSetting(false, 1);
+        }
+    }
+
+
+    function initFadeSetting(loaded, num) {
+        console.log("initFadeSetting: loaded is ", loaded);
+
+        var win = $(window),
+        win_h = win.height(),
+        scrl_top = 0;
+
+        var scrl_show = function(){
+            var e = $('#links img'),
+                arr = [],
+                flag = [];
+            console.log(e);
+
+            // 対象の要素を表示する際の演出
+            var e_show_anime = function(i){
+                arr[i].elm.stop().fadeIn(1000);
+            };
+            
+            // 対象の要素を表示するかどうか判別
+            var e_show = function(i){
+                console.log("e_show: ", arr[i].top);
+                if(scrl_top > arr[i].top - win_h * 0.75){ // 表示位置の基準値を調整する場合はココ
+                    e_show_anime(i);
+                    flag[i] = true;
+                }
+            };
+
+            // 対象の要素を取得
+            if(loaded == true) {
+                e.each(function(i){
+                    $(this).bind("load", function() {
+                        console.log("image loaded");
+                        flag[i] = false;
+                        console.log($(this).height());
+                        arr[i] = {
+                            elm : $(this),
+                            top : $(this).offset().top + ($(this).height() * i)
+                        };
+                        arr[i].elm.hide(); // cssで要素を非表示にしない場合は必要
+                        console.log("arr: ", arr);
+
+                        win.scroll(function(){
+                            if( flag[i] === false ){
+                                e_show(i);
+                            }
+                        });
+
+                        e_show(i);
+                    });
+                });
+            }
+            else {
+                e.each(function(i){
+                    console.log("loaded is false");
+                    flag[i] = false;
+                    console.log($(this).height());
+                    arr[i] = {
+                        elm : $(this),
+                        top : $(this).offset().top + ($(this).height() * i / num)
+                    };
+                    arr[i].elm.hide(); // cssで要素を非表示にしない場合は必要
+                    console.log("arr: ", arr);
+
+                    win.scroll(function(){
+                        if( flag[i] === false ){
+                            e_show(i);
+                        }
+                    });
+
+                    e_show(i);
+                });
+            }
+        };
+
+        $('document').ready(function(){
+            scrl_top = win.scrollTop();
+
+            win.resize(function(){
+                win_h = win.height();
+                console.log( win_h );
+            });
+            win.scroll(function(){
+                scrl_top = win.scrollTop();
+            });
+
+            scrl_show();
         });
     }
 
